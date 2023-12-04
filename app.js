1abc51d85c501a8a5d86df25112fe99783892489
Serena <serena@christophersmbp.lan>
2023-12-04 12:33:34 -0800

    deploy setting

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import cors from "cors";
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://127.0.0.1:27017/kanbas');
+// mongoose.connect('mongodb://127.0.0.1:27017/kanbas');
 import UserRoutes from './users/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import AssignmentRoutes from './assignments/routes.js';
 import session from "express-session";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
+
+
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.FONTEND_URL
+    origin: 'https://a6--kanbas-not-canvas-1214.netlify.app'
 }
 ));
 const sessionOptions = {
